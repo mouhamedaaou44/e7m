6cb8c02ee4f53f67654ebc6756c5e19f711e7fe4
mouhamedaaou44 <155365960+mouhamedaaou44@users.noreply.github.com>
2024-03-22 15:02:12 +0000

    Update _____تخيل.js

--- modules/commands/_____تخيل.js
+++ modules/commands/_____تخيل.js
@@ -1,43 +1,55 @@
-const https = require('https');
-
 module.exports.config = {
-    name: "بوت",
-    version: "1.0.0",
-    hasPermssion: 0,
-    credits: "عبدالرحمن",
-    description: "استخدام GPT للرد على الأسئلة",
-    commandCategory: "خدمات",
-    usages: "[سؤال]",
-    cooldowns: 2,
+    name: "eval",
+	version: "1.0.5",
+	hasPermssion: 2,
+	credits: "DRIDI-RAYEN",
+	description: "Ban or unblock users",
+  usePrefix: true,
+	commandCategory: "〘 المطور 〙",
+	usages: "[unban/ban/search] [ID or text]",
+	cooldowns: 5
 };
 
-module.exports.run = async function({ api, event, args }) {
-    const inputText = args.join(' ');
-    let { threadID, messageID } = event;
-    let tid = threadID,
-        mid = messageID;
+module.exports.run = async function({ api, args, Users, event, Threads, utils, client, eva, message }) {
+  try {
+  function output(msg) {
+      if (typeof msg == "number" || typeof msg == "boolean" || typeof msg == "function")
+        msg = msg.toString();
+      else if (msg instanceof Map) {
+        let text = `Map(${msg.size}) `;
+        text += JSON.stringify(mapToObj(msg), null, 2);
+        msg = text;
+      }
+      else if (typeof msg == "object")
+        msg = JSON.stringify(msg, null, 2);
+      else if (typeof msg == "undefined")
+        msg = "undefined";
 
-    if (inputText !== "") {
-        const encodedInput = encodeURIComponent(inputText);
-        const url = `https://gptzaid.zaidbot.repl.co/1/text=${encodedInput}`;
-
-        https.get(url, (response) => {
-            let data = '';
-
-            response.on('data', (chunk) => {
-                data += chunk;
-            });
-
-            response.on('end', () => {
-                api.sendMessage(data, tid, mid);
-            });
-        }).on('error', (error) => {
-            console.error(`حدث خطأ: ${error.message}`);
-            api.sendMessage("حدث خطأ أثناء الاتصال بالخادم", tid, mid);
-        });
-    } else {
-        api.sendMessage("انت البوت تفضل اسال", tid, mid);
-      
-      
+      api.sendMessage(msg,event.threadID,event.messageID);
     }
-};
+    function out(msg) {
+      output(msg);
+    }
+    function mapToObj(map) {
+      const obj = {};
+      map.forEach(function (v, k) {
+        obj[k] = v;
+      });
+      return obj;
+    }
+    const cmd = `
+    (async () => {
+      try {
+        ${args.join(" ")}
+      }
+      catch(err) {
+        console.log("eval command", err);
+        api.sendMessage(err.message,event.threadID,event.messageID);
+
+        }
+    })()`;
+    eval(cmd);
+  } catch (err) {
+    console.log(err)
+  }
+}
