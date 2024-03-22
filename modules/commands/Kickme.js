ea6bc40c196c3f885d87ab80236b9a2ad0601b9c
mouhamedaaou44 <155365960+mouhamedaaou44@users.noreply.github.com>
2024-03-22 14:29:38 +0000

    Update Kickme.js

--- modules/commands/Kickme.js
+++ modules/commands/Kickme.js
@@ -4,7 +4,7 @@ version: "1.0.0",
 hasPermssion: 0,
 credits: "D-Jukie",
 description: "يطردك من المجموعة",
-commandCategory: "اشياء",
+commandCategory: "〘 المجموعات 〙",
 usages: "  اذا انت ادمن ومتريد الامر يشتغل ببساطه نزل البوت من الادمن",
 cooldowns: 3
 }; 
