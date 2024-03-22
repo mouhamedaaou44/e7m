ab63ab16a26de787a2509512485a8d8bfd233eb5
mouhamedaaou44 <155365960+mouhamedaaou44@users.noreply.github.com>
2024-03-22 15:20:20 +0000

    Update adbot.js

--- modules/commands/adbot.js
+++ modules/commands/adbot.js
@@ -4,7 +4,7 @@ module.exports.config = {
     hasPermssion: 0,
     credits: "𝐊𝐈𝐓𝐄 凧",
     description: "",
-    commandCategory: "معلومات",
+    commandCategory: "〘 النظام 〙",
     usages: "add bot",
     cooldowns: 4,
     dependencies: {
