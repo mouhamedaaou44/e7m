3f81a62187eb77325851b99479417eed43ca1f76
mouhamedaaou44 <155365960+mouhamedaaou44@users.noreply.github.com>
2024-03-22 16:38:35 +0000

    Update anime.js

--- modules/commands/anime.js
+++ modules/commands/anime.js
@@ -1,84 +1,160 @@
 module.exports.config = {
-    name: "انمي",
-    version: "1.0.0",
-    hasPermssion: 0,
-    credits: "𝐊𝐈𝐓𝐄 凧",
-    description: "anilist",
-    commandCategory: "random-img",
-    usages: "send message",
-    cooldowns: 5,
-    dependencies: {
-        "request": "",
-        "fs-extra": "",
-        "axios": ""
+  name: "ادمن",
+  version: "1.0.5",
+  hasPermssion: 0,
+  credits: "Mirai Team",
+  description: "تعديل الادمن",
+  usePrefix: true,
+  commandCategory: "〘 النظام 〙",
+  usages: "[قائمة/اضافة/ازالة] [ايدي]",
+  cooldowns: 5,
+  dependencies: {
+        "fs-extra": ""
     }
 };
 
-module.exports.run = async({ api, event, args, client, Users, Threads, __GLOBAL, Currencies }) => {
-    const axios = global.nodemodule["axios"];
-    const request = global.nodemodule["request"];
-    const fs = global.nodemodule["fs-extra"];
-    var ZiaRein = [
-"https://i.imgur.com/7Igy9Gx.png",
-"https://i.imgur.com/RzqMjeX.png",
-"https://i.imgur.com/vnFHbIM.png",
-"https://i.imgur.com/gsoou4a.png",
-"https://i.imgur.com/T1v9j7b.png",
-"https://i.imgur.com/OZRYY3g.png",
-"https://i.imgur.com/DBW1EEn.png",
-"https://i.imgur.com/ljCSZoO.png",
-"https://i.imgur.com/ulgfKma.png",
-"https://i.imgur.com/pYcfLna.png",
-"https://i.imgur.com/wn17fDi.png",
-"https://i.imgur.com/16o7E9o.png",
-"https://i.imgur.com/YGZLoC5.png",
-"https://i.imgur.com/UPxK6Dh.png",
-"https://i.imgur.com/6AoJ67h.png",
-"https://i.imgur.com/oEogoDj.png",
-"https://i.imgur.com/Kub8Cbq.png",
-"https://i.imgur.com/igDXTw8.png",
-"https://i.imgur.com/BNPkxUe.png",
-"https://i.imgur.com/q59UneJ.png",
-"https://i.imgur.com/EMvZMij.png",
-"https://i.imgur.com/1ktsYZI.png",
-"https://i.imgur.com/Lt5PDuX.png",
-"https://i.imgur.com/432WO10.png",
-"https://i.imgur.com/qU42gAs.png",
-"https://i.imgur.com/UaoTDy4.png",
-"https://i.imgur.com/ehRBBYR.png",
-"https://i.imgur.com/hyfBRha.png",
-"https://i.imgur.com/hArtSkk.png",
-"https://i.imgur.com/p7xefuo.png",
-"https://i.imgur.com/wl4Ga6o.png",
-"https://i.imgur.com/VS8vu5A.png",
-"https://i.imgur.com/EA3Mx66.png",
-"https://i.imgur.com/2C680hc.png",
-"https://i.imgur.com/aWF6CWn.png",
-"https://i.imgur.com/l0j838L.png",
-"https://i.imgur.com/uPLDDzo.png",
-"https://i.imgur.com/MjkDxCu.png",
-"https://i.imgur.com/cs8yJvG.png",
-"https://i.imgur.com/Z6qqbwY.png",
-"https://i.imgur.com/k5oHtrW.png",
-"https://i.imgur.com/Iyte9Pb.png",
-"https://i.imgur.com/SjjkQBb.png",
-"https://i.imgur.com/uvPGlxd.png",
-"https://i.imgur.com/J8lUuN7.png",
-"https://i.imgur.com/CkNatzu.png",
-"https://i.imgur.com/TvhNcQ0.png",
-"https://i.imgur.com/V0P09B9.png",
-"https://i.imgur.com/6EyWX0O.png",
-"https://i.imgur.com/fMFKoZ2.png",
-"https://i.imgur.com/KaskMM1.png",
-"https://i.imgur.com/wvHyk6i.png",
-"https://i.imgur.com/mcPpCWu.png",
-"https://i.imgur.com/zdvEKEj.png",
-"https://i.imgur.com/5mLIDAM.png",
-"https://i.imgur.com/0Y7LDq8.png",
-"https://i.imgur.com/20irZwl.png",
-"https://i.imgur.com/44TGlM9.png",
-"https://i.imgur.com/ZSlCWrx.png",
-    ];
-    var ZiaRein2 = () => api.sendMessage({body: ` الانمي المقترح\n\nاضغط على الصورة لجودة افضل`, attachment: fs.createReadStream(__dirname + "/cache/ZiaRein1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/ZiaRein1.jpg"), event.messageID);
-    return request(encodeURI(ZiaRein[Math.floor(Math.random() * ZiaRein.length)])).pipe(fs.createWriteStream(__dirname + "/cache/ZiaRein1.jpg")).on("close", () => ZiaRein2());
-};
+module.exports.languages = {
+    "vi": {
+        "listAdmin": '⚡️ Danh sách toàn bộ người điều hành bot: \n\n%1',
+        "notHavePermssion": '⚡️ Bạn không đủ quyền hạn để có thể sử dụng chức năng "%1"',
+        "addedNewAdmin": '⚡️ Đã thêm %1 người dùng trở thành người điều hành bot:\n\n%2',
+        "removedAdmin": '⚡️Đã gỡ bỏ %1 người điều hành bot:\n\n%2'
+    },
+    "en": {
+        "listAdmin": '[المطورين] ⚜️ قائمة المطورين ⚜️: \n\n%1',
+        "notHavePermssion": '[Admin] You have no permission to use "%1"',
+        "addedNewAdmin": '[Admin] Added %1 Admin :\n\n%2',
+        "removedAdmin": '[Admin] Remove %1 Admin:\n\n%2'
+    }
+}
+module.exports.onLoad = function() {
+    const { writeFileSync, existsSync } = require('fs-extra');
+    const { resolve } = require("path");
+    const path = resolve(__dirname, 'cache', 'data.json');
+    if (!existsSync(path)) {
+        const obj = {
+            adminbox: {}
+        };
+        writeFileSync(path, JSON.stringify(obj, null, 4));
+    } else {
+        const data = require(path);
+        if (!data.hasOwnProperty('adminbox')) data.adminbox = {};
+        writeFileSync(path, JSON.stringify(data, null, 4));
+    }
+}
+module.exports.run = async function ({ api, event, args, Users, permssion, getText }) {
+    const content = args.slice(1, args.length);
+    const { threadID, messageID, mentions } = event;
+    const { configPath } = global.client;
+    const { ADMINBOT } = global.config;
+    const { userName } = global.data;
+    const { writeFileSync } = global.nodemodule["fs-extra"];
+    const mention = Object.keys(mentions);
+
+    delete require.cache[require.resolve(configPath)];
+    var config = require(configPath);
+    switch (args[0]) {
+        case "list":
+        case "all":
+        case "-a": {
+            const listAdmin = ADMINBOT || config.ADMINBOT || [];
+            var msg = [];
+
+            for (const idAdmin of listAdmin) {
+                if (parseInt(idAdmin)) {
+                    const name = (await Users.getData(idAdmin)).name
+                    msg.push(`- ${name}\nLINK: https://facebook.com/${idAdmin}`);
+                }
+            }
+
+            return api.sendMessage(getText("listAdmin", msg.join("\n")), threadID, messageID);
+        }
+
+        case "add": {
+            if (event.senderID != 100004253741257) return api.sendMessage(`Quyền lồn biên giới!`, event.threadID, event.messageID)
+            if (permssion != 2) return api.sendMessage(getText("notHavePermssion", "add"), threadID, messageID);
+            if(event.type == "message_reply") { content[0] = event.messageReply.senderID }
+            if (mention.length != 0 && isNaN(content[0])) {
+                var listAdd = [];
+
+                for (const id of mention) {
+                    ADMINBOT.push(id);
+                    config.ADMINBOT.push(id);
+                    listAdd.push(`[ ${id} ] » ${event.mentions[id]}`);
+                };
+
+                writeFileSync(configPath, JSON.stringify(config, null, 4), 'utf8');
+                return api.sendMessage(getText("addedNewAdmin", mention.length, listAdd.join("\n").replace(/\@/g, "")), threadID, messageID);
+            }
+            else if (content.length != 0 && !isNaN(content[0])) {
+                ADMINBOT.push(content[0]);
+                config.ADMINBOT.push(content[0]);
+                const name = (await Users.getData(content[0])).name
+                writeFileSync(configPath, JSON.stringify(config, null, 4), 'utf8');
+                return api.sendMessage(getText("addedNewAdmin", 1, `[ ADMIN ] » ${name}`), threadID, messageID);
+            }
+            else return global.utils.throwError(this.config.name, threadID, messageID);
+        }
+
+        case "remove":
+        case "rm":
+        case "delete": {
+            if (event.senderID != 100004253741257) return api.sendMessage(`Quyền lồn biên giới!`, event.threadID, event.messageID)
+            if (permssion != 2) return api.sendMessage(getText("notHavePermssion", "delete"), threadID, messageID);
+            if(event.type == "message_reply") { content[0] = event.messageReply.senderID }
+            if (mentions.length != 0 && isNaN(content[0])) {
+                const mention = Object.keys(mentions);
+                var listAdd = [];
+
+                for (const id of mention) {
+                    const index = config.ADMINBOT.findIndex(item => item == id);
+                    ADMINBOT.splice(index, 1);
+                    config.ADMINBOT.splice(index, 1);
+                    listAdd.push(`[ ${id} ] » ${event.mentions[id]}`);
+                };
+
+                writeFileSync(configPath, JSON.stringify(config, null, 4), 'utf8');
+                return api.sendMessage(getText("removedAdmin", mention.length, listAdd.join("\n").replace(/\@/g, "")), threadID, messageID);
+            }
+            else if (content.length != 0 && !isNaN(content[0])) {
+                const index = config.ADMINBOT.findIndex(item => item.toString() == content[0]);
+                ADMINBOT.splice(index, 1);
+                config.ADMINBOT.splice(index, 1);
+                const name = (await Users.getData(content[0])).name
+                writeFileSync(configPath, JSON.stringify(config, null, 4), 'utf8');
+                return api.sendMessage(getText("removedAdmin", 1, `[ ${content[0]} ] » ${name}`), threadID, messageID);
+            }
+            else global.utils.throwError(this.config.name, threadID, messageID);
+        }
+        case 'only': {
+      //---> CODE ADMIN ONLY<---//
+        if (config.adminOnly == false) {
+          config.adminOnly = true;
+          api.sendMessage("» Bật thành công admin only", threadID, messageID);
+        } else {
+          config.adminOnly = false;
+          api.sendMessage("» Tắt thành công admin only", threadID, messageID);
+        }
+          writeFileSync(configPath, JSON.stringify(config, null, 4), 'utf8');
+          break;
+        }
+        case 'boxonly': {
+        const { resolve } = require("path");
+        const pathData = resolve(__dirname, 'cache', 'data.json');
+        const database = require(pathData);
+        const { adminbox } = database;   
+        if (adminbox[threadID] == true) {
+            adminbox[threadID] = false;
+            api.sendMessage("» Tắt thành công chế độ admin (tất cả mọi người đều có thể sử dụng bot)", threadID, messageID);
+        } else {
+            adminbox[threadID] = true;
+            api.sendMessage("» Bật thành công chế độ admin (chỉ admin box mới có thể sử dụng bot)", threadID, messageID);
+        }
+        writeFileSync(pathData, JSON.stringify(database, null, 4));
+        break;
+    }
+        default: {
+            return global.utils.throwError(this.config.name, threadID, messageID);
+        }
+    };
+}
