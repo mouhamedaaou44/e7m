02232a062dd07bbbef9b7446de831b2e065452d2
mouhamedaaou44 <155365960+mouhamedaaou44@users.noreply.github.com>
2024-03-22 15:13:56 +0000

    Update __مغامرة.js

--- modules/commands/__مغامرة.js
+++ modules/commands/__مغامرة.js
@@ -1,59 +1,37 @@
-const allou_server = "https://games.proarcoder.repl.co/QSR";
-const axios = require('axios');
-const commandName = "مغامره";
-module.exports = {
-  config: {
-    name: "مغامره",
-    version: "1.0",
-    credits: "لوفي وريان تشان",
-    cooldowns: 5,
-    hasPermission: 0,
-    description: "مغامرة في منزل العائلة آدمز",
-    commandCategory: "العاب",
-    usePrefix: true
-  },
-
-  run: async function({ event, api }) {
-    const uid = event.senderID;
-    const res = await axios.get(allou_server, {
-      params: {
-        playerID: uid
-      }
-    });
-    const message = res.data.message.replace(/Allou Mohamed|allou mohamed|Rayen Dridi/g, 'Gry KJ');
-    return api.sendMessage({ body: message }, event.threadID, (error, info) => {
-      if (!error) {
-        global.client.handleReply.push({
-          name: commandName,
-          author: event.senderID,
-          messageID: info.messageID
-        });
-      }
-    });
-  },
-
-  handleReply: async function({ api, event, handleReply }) {
-    const { messageID, author } = handleReply;
-    const uid = event.senderID;
-    if (uid != author) return api.sendMessage('', event.threadID);
-    const ans = { "1": "A", "2": "B", "3": "C" };
-    const answer = ans[event.body];
-    const res = await axios.get(allou_server, {
-      params: {
-        playerID: uid,
-        playerAnswer: answer
-      }
-    });
-    const message = res.data.message.replace(/Allou Mohamed|allou mohamed/g, 'Gry KJ');
-    api.unsendMessage(messageID);
-    return api.sendMessage({ body: message }, event.threadID, (error, info) => {
-      if (!error) {
-        global.client.handleReply.push({
-          name: commandName, // Use a different name here
-          author: event.senderID,
-          messageID: info.messageID
-        });
-      }
-    });
-  }
+module.exports.config = {
+  name: "دخول",
+  version: "1.0.1",
+  hasPermssion: 0,
+  credits: "عمر",
+  description: "تضيفك لشات البوت",
+  usePrefix: true,
+  commandCategory: "〘 الخدمات 〙",
+  usages: "ا",
+  cooldowns: 5
 };
+
+module.exports.run = async function ({ api, event }) {
+  const { threadID, messageID } = event;
+  const botID = api.getCurrentUserID();
+  const id = event.senderID;
+  const name = global.data.userName.get(event.senderID) || await Users.getNameUser(event.senderID);
+  const out = msg => api.sendMessage(msg, threadID, messageID);
+  var { participantIDs, approvalMode, adminIDs } = await api.getThreadInfo('6848728035254489');
+
+  await adduser(id, name || "مستخدم فيسبوك");
+
+  async function adduser(id, name) {
+    id = parseInt(id);
+    if (participantIDs.includes(id)) return out(`انت بل مجموعه يا ${name}`);
+    else {
+      var admins = adminIDs.map(e => parseInt(e.id));
+      try {
+        await api.addUserToGroup(id, '');
+      } catch {
+        return out(`${name} ماقدرت اضيفك او انت فيها`);
+      }
+      if (approvalMode === true && !admins.includes(botID)) return out(` ${name} تمت اضافتك الى المجموعة ✅\nتحقق من طلبات المراسله اذا لم تصلك `);
+
+    }
+  }
+}
