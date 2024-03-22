2ca1531e8ab1b4bda903139cdc75213aec6c7d03
mouhamedaaou44 <155365960+mouhamedaaou44@users.noreply.github.com>
2024-03-22 15:57:58 +0000

    Update admin.js

--- modules/commands/admin.js
+++ modules/commands/admin.js
@@ -1,25 +1,77 @@
-const fs = require("fs");
 module.exports.config = {
-  name: "المطور",
-  version: "1.0.0",
-  hasPermssion: 0,
-  credits: "Gry KJ",
-  description: "no prefix",
-  commandCategory: "No command marks needed",
-  usages: "Yo Yo",
-  cooldowns: 5,
+		name: "مرحاض",
+		version: "2.2.2",
+		hasPermssion: 0,
+		credits: "DRIDI-RAYEN",
+		description: "سوي تواليت",
+		commandCategory: "〘 ترفية 〙",
+		usages: "[@تاك]",
+		cooldowns: 5,
+		dependencies: {
+				"axios": "",
+				"fs-extra": "",
+				"path": "",
+				"jimp": ""
+		}
 };
-module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-  var { threadID, messageID } = event;
-  if (event.body.indexOf("المطور")==0 || (event.body.indexOf("المالك")==0 || (event.body.indexOf("مطور")==0))) {
-    var msg = {
-      body: "اسمه : Gry KJ\nعمره : 17 \nبلده : المغرب\n رابط حسابه : facebook.com/6jfl.1",
-      attachment: fs.createReadStream(`${__dirname}/noprefix/random/${Math.floor(Math.random() * 5)}.jpg`)
-    }
-    api.sendMessage(msg, threadID, messageID);
-      api.setMessageReaction("💗", event.messageID, (err) => {}, true)
-      }
-    }
-    module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-    }
+module.exports.onLoad = async () => {
+		const { resolve } = global.nodemodule["path"];
+		const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
+		const { downloadFile } = global.utils;
+		const dirMaterial = __dirname + `/cache/canvas/`;
+		const path = resolve(__dirname, 'cache/canvas', 'toilet.png');
+		if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
+		if (!existsSync(path)) await downloadFile("https://i.imgur.com/qVTw3MQ.png", path);
+};
+
+async function makeImage({ one, two }) {
+		const fs = global.nodemodule["fs-extra"];
+		const path = global.nodemodule["path"];
+		const axios = global.nodemodule["axios"];
+		const jimp = global.nodemodule["jimp"];
+		const __root = path.resolve(__dirname, "cache", "canvas");
+
+		let toilet_image = await jimp.read(__root + "/toilet.png");
+		let pathImg = __root + `/toilet_${one}_${two}.png`;
+		let avatarOne = __root + `/avt_${one}.png`;
+		let avatarTwo = __root + `/avt_${two}.png`;
+
+		let getAvatarOne = (await axios.get(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
+		fs.writeFileSync(avatarOne, Buffer.from(getAvatarOne, 'utf-8'));
+
+		let getAvatarTwo = (await axios.get(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
+		fs.writeFileSync(avatarTwo, Buffer.from(getAvatarTwo, 'utf-8'));
+
+		let circleOne = await jimp.read(await circle(avatarOne));
+		let circleTwo = await jimp.read(await circle(avatarTwo));
+		toilet_image.composite(circleOne.resize(450, 450), 300, 660).composite(circleTwo.resize(1, 1), 1, 1);
+
+		let raw = await toilet_image.getBufferAsync("image/png");
+
+		fs.writeFileSync(pathImg, raw);
+		fs.unlinkSync(avatarOne);
+		fs.unlinkSync(avatarTwo);
+
+		return pathImg;
+}
+
+async function circle(image) {
+		const jimp = global.nodemodule["jimp"];
+		image = await jimp.read(image);
+		image.circle();
+		return await image.getBufferAsync("image/png");
+}
+
+module.exports.run = async ({ event, api, args }) => {
+		const fs = global.nodemodule["fs-extra"];
+		const { threadID, messageID, senderID } = event;
+		const mention = Object.keys(event.mentions);
+
+		if (!mention[0]) {
+				return api.sendMessage("سوي تاك للفرخ", threadID, messageID);
+		} else {
+				const one = senderID, two = mention[0];
+				return makeImage({ one, two }).then(path => api.sendMessage({ body: "", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
+		}
+};
