b1205220bf99633d7c15d2f4988520452bd39b22
mouhamedaaou44 <155365960+mouhamedaaou44@users.noreply.github.com>
2024-03-25 14:44:19 +0000

    Update adden.js

--- modules/commands/adden.js
+++ modules/commands/adden.js
@@ -1,10 +1,10 @@
 const axios = require('axios');
 module['exports']['config'] = {
-    name: "ارسمي",
+    name: "بكسارت",
     version: "1.0.0",
     hasPermssion: 0,
     credits: "",
-    description: " رسم الصور (DALLE).",
+    description: "تخيل الصور.",
 usePrefix: false,
     commandCategory: "〘 الذكاء الاصطناعي  〙",
     usages: "[اِسْتَدْعَى | نموذج]",
@@ -16,24 +16,24 @@ const { get } = require("axios");
 
 module['exports']['run'] = async function({ api, event, args }) {
     let path = __dirname + "/cache/image.png";
-    const tzt = args.join(" ").split("").map(item => item.trim());
+    const tzt = args.join(" ").split("|").map(item => item.trim());
     let txt = tzt[0];
-    let txt2 = .;
+    let txt2 = tzt[1];
 
     let tid = event.threadID;
     let mid = event.messageID;
 
-    if (!args[0] || !txt || !txt2) return api.sendMessage("يرجى تقديم موجه ونموذج.", tid, mid);
-  
+    if (!args[0] || !txt || !txt2) return api.sendMessage("يرجى تقديم موجه ونموذج من 1 إلى 7.", tid, mid);
+
     try {
- 
+
         api.sendMessage("⏳توليد ...", tid, mid);
 const tranChat = await axios.get(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=ja&dt=t&q=${encodeURIComponent(txt)}`);
 
 var zaba = tranChat.data[0][0][0];
-      
+
         let enctxt = encodeURI(zaba);
-        let url = `https://deku-rest-api.replit.app/dalle?prompt=${enctxt}`;
+        let url = `https://deku-rest-api.replit.app/pixart?prompt=${enctxt}&styles=${txt2}`;
 
         let result = (await get(url, { responseType: "arraybuffer" })).data;
         fs.writeFileSync(path, Buffer.from(result, "utf-8"));
